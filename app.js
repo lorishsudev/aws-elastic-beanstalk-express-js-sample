f5677482c6201f4b8fcd6189483c87712a89abc8
Loris Hsu 許銘欽 <loris.hsu@lcygroup.com>
2024-02-29 11:29:29 +0800

    20240229 V02

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey ! This is my CI/CD pipeline using AWS'));
+app.get('/', (req, res) => res.send('Hey ! This is my CI/CD pipeline using AWS V2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
